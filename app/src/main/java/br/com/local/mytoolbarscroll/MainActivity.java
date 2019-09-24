86037807e359596a388d15c8e86ea9fff55d2bab
lndsilva <lndsilva@hotmail.com>
2019-09-24 17:17:44 -0300

    inserindo collapsing image scroll FAB

--- app/src/main/java/br/com/local/mytoolbarscroll/MainActivity.java
+++ app/src/main/java/br/com/local/mytoolbarscroll/MainActivity.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.idCollToolBar);
-        collapsingToolbar.setTitle("Título de Barra Scroll");
+        toolbar = findViewById(R.id.idToolBar);
+        setSupportActionBar(toolbar);
 
     }
 }
